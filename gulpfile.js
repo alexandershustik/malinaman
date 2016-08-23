9430fabdddee78abfd1469ee533468aad5a2df64
Alexander Shustik <alexandershustik@gmail.com>
2016-08-23 18:07:00 +0200

    подключил bootstrap
    jade, gulp, bootstrap, sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var gulp       = require('gulp'),
+var gulp         = require('gulp'),
     sass         = require('gulp-sass'),
     browserSync  = require('browser-sync'),
     concat       = require('gulp-concat'),
@@ -9,7 +9,8 @@ var gulp       = require('gulp'),
     imagemin     = require('gulp-imagemin'),
     pngquant     = require('imagemin-pngquant'),
     cache        = require('gulp-cache'),
-    autoprefixer = require('gulp-autoprefixer');
+    autoprefixer = require('gulp-autoprefixer'),
+    jade         = require('gulp-jade');
 
 gulp.task('sass', function(){
     return gulp.src('app/sass/**/*.sass')
@@ -17,6 +18,12 @@ gulp.task('sass', function(){
         .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
         .pipe(gulp.dest('app/css'))
         .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('jade', function(){
+    gulp.src('app/templates/**/*.jade')
+        .pipe(jade())
+        .pipe(gulp.dest('app/'))
 });
 
 gulp.task('browser-sync', function() {
@@ -31,7 +38,7 @@ gulp.task('browser-sync', function() {
 gulp.task('scripts', function() {
     return gulp.src([
         'app/libs/jquery/dist/jquery.min.js',
-        'app/libs/inputmask/inputmask.min.js'
+        'app/libs/bootstrap/dist/js/bootstrap.min.js'
         ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
@@ -47,6 +54,7 @@ gulp.task('css-libs', ['sass'], function() {
 
 gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
     gulp.watch('app/sass/**/*.sass', ['sass']);
+    gulp.watch('app/templates/**/*.jade',['jade']);
     gulp.watch('app/*.html', browserSync.reload);
     gulp.watch('app/js/**/*.js', browserSync.reload);
 });
